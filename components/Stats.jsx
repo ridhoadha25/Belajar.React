bae65d9e94653bc6258fbd228368ee39d2dda051
M.Ridho Adha <mridhoadha25@gmail.com>
2026-06-15 20:39:40 +0700

    Update Stats.jsx

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -14,16 +14,19 @@ function Stats({ todos }) {
     <div className="stats">
 
       <div>
+        <i className="fas fa-tasks" style={{fontSize: "1.8em", color: "#3b82f6", marginBottom: "10px"}}></i>
         <h3>{total}</h3>
         <p>Total</p>
       </div>
 
       <div>
+        <i className="fas fa-spinner" style={{fontSize: "1.8em", color: "#3b82f6", marginBottom: "10px"}}></i>
         <h3>{active}</h3>
         <p>Aktif</p>
       </div>
 
       <div>
+        <i className="fas fa-check" style={{fontSize: "1.8em", color: "#3b82f6", marginBottom: "10px"}}></i>
         <h3>{completed}</h3>
         <p>Selesai</p>
       </div>
